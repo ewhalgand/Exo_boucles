1a9efab2d1aac3a8b8ece5de569bac81f57aaf29
ewhalgand <halgandewen44@gmail.com>
2025-03-05 09:52:33 +0100

    Initial Commit

---
+++ Exo1/main.js
@@ -0,0 +1,23 @@
+// 1.
+for (let i = 0; i < 100; i++) {
+    console.log(i)
+}
+
+// 2.
+let num = 0
+
+while (num <= 99) {
+    console.log(num++)
+}
+
+// 3.
+for (let i = 50; i < 100; i++) {
+    console.log(i);
+}
+
+// 4.
+let num2 = 49;
+
+while (num2 >= 1) {
+    console.log(num2--)
+}
