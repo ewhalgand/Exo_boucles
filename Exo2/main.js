604cb69701caedb673ebd7480062a2f0c2811bd3
ewhalgand <halgandewen44@gmail.com>
2025-03-05 10:01:30 +0100

    add Exo2

---
+++ Exo2/main.js
@@ -0,0 +1,8 @@
+// let letters = [ 'A', 'd', 'a', '-', 'T', 'e', 'c', 'h']
+let letters = [ 'A', 'd', 'a', '-', 'S', 'c', 'h', 'o', 'o', 'l']
+let message = "";
+
+for (const letter of letters) {
+    message += letter
+}
+console.log(message)
